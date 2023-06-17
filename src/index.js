2a1c9c5af92b54e9d175e3809d4e683a96d2fa1f
Damir Tagilbayev <93281431+damirTAG@users.noreply.github.com>
2023-06-17 07:06:05 +0600

    openai integration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 require("dotenv").config();
 const { Client, IntentsBitField, ActivityType } = require("discord.js");
+/*
+openai
+const { Configuration, OpenAIApi } = require('openai');
+*/
 const eventHandler = require("./handlers/eventHandler");
 const listWords = require("./data/list");
 
@@ -34,4 +38,56 @@ client.on("messageCreate", (message) => {
             break;
         }
     }
+    /* OPENAI START
+      if (message.channel.id !== process.env.CHANNEL_ID) return;
+      if (message.content.startsWith('!')) return;
+
+      let conversationLog = [
+        { role: 'system', content: 'You are a friendly chatbot.' },
+      ];
+
+      try {
+        await message.channel.sendTyping();
+        let prevMessages = await message.channel.messages.fetch({ limit: 15 });
+        prevMessages.reverse();
+
+        prevMessages.forEach((msg) => {
+          if (msg.content.startsWith('!')) return;
+          if (msg.author.id !== client.user.id && message.author.bot) return;
+          if (msg.author.id == client.user.id) {
+            conversationLog.push({
+              role: 'assistant',
+              content: msg.content,
+              name: msg.author.username
+                .replace(/\s+/g, '_')
+                .replace(/[^\w\s]/gi, ''),
+            });
+          }
+
+          if (msg.author.id == message.author.id) {
+            conversationLog.push({
+              role: 'user',
+              content: msg.content,
+              name: message.author.username
+                .replace(/\s+/g, '_')
+                .replace(/[^\w\s]/gi, ''),
+            });
+          }
+        });
+
+        const result = await openai
+          .createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: conversationLog,
+            // max_tokens: 256, // limit token usage
+          })
+          .catch((error) => {
+            console.log(`OPENAI ERR: ${error}`);
+          });
+        message.reply(result.data.choices[0].message);
+      } catch (error) {
+        console.log(`ERR: ${error}`);
+      }
+    });
+    OPENAI END*/
 });
