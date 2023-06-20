49e257ba0b1be668b86232e4ce1a1f2c25f39488
Damir Tagilbayev <93281431+damirTAG@users.noreply.github.com>
2023-06-21 03:13:00 +0600

    new command /avatar

---
+++ src/commands/misc/avatar.js
@@ -0,0 +1,31 @@
+const { EmbedBuilder, ApplicationCommandOptionType } = require("discord.js");
+
+module.exports = {
+    name: "avatar",
+    description: "Показывает аватар выбранного пользователя",
+    options: [
+        {
+            name: "mention",
+            description: "Это пользователь, аватар которого вы хотите отобразить",
+            premissions: "0x0000000000000800",
+            required: true,
+            type: ApplicationCommandOptionType.User,
+        },
+    ],
+    // devOnly: true,
+    // testOnly: true,
+    // options: Object[],
+    callback: async (client, interaction) => {
+        const avatarmention = interaction.options.getUser("mention");
+        if (avatarmention) {
+            embed = new EmbedBuilder()
+                .setTitle(`Аватар @${avatarmention.username}`)
+                .setColor("Random")
+                .setImage(`${avatarmention.displayAvatarURL({ size: 1024, format: "png", dynamic: true })}`)
+                .setFooter({ text: "made by ©damirTAG", iconURL: `${interaction.user.displayAvatarURL()}` });
+        }
+        return interaction.reply({
+            embeds: [embed],
+        });
+    },
+};
